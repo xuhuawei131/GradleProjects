2fbb3e47a902a3f0aef627812c8c0f1a4784636c
xuhuawei <xuhw@knowbox.cn>
2019-03-29 01:07:18 +0800

    sourceSet

---
+++ GradleDemo13/pullalive/src/main/java/com/xuhuawei/pullalive/MainPullAliveActivity.java
@@ -0,0 +1,13 @@
+package com.xuhuawei.pullalive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainPullAliveActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_pull_alive);
+    }
+}
